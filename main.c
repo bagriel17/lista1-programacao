a066be5a11cf9a7651b4e0dcf0906b9dcf3db3e9
bagriel17 <gabrielbastosmelo7@gmail.com>
2024-08-06 09:13:46 -0300

    main.c

--- main.c
+++ main.c
@@ -5,13 +5,13 @@ int main()
 {
 	int num1, num2, sum;
 
-	printf("Selecione o primeiro dC-gito: ");
+	printf("Selecione o primeiro digito: ");
 	scanf("%d", &num1);
-	printf("Selecione o segundo dC-gito: ");
+	printf("Selecione o segundo digito: ");
 	scanf("%d", &num2);
 	sum = num1 + num2;
 
-	printf("A soma C) %d + %d = %d", num1, num2, sum);
+	printf("A soma e) %d + %d = %d", num1, num2, sum);
 
 	return 0;
 }
