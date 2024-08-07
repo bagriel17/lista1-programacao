d0156c467a1beadc63006cebf4c84b81c39c52b9
bagriel17 <gabrielbastosmelo7@gmail.com>
2024-08-07 01:12:11 -0300

    main1.c

---
+++ main (1).c
@@ -0,0 +1,17 @@
+//SOMA - PROGRAMAÇÃO (01/40)
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, soma;
+    printf("Insira o primeiro digito: \n");
+    scanf("%d", &num1);
+    printf("Insira o segundo digito: \n");
+    scanf("%d", &num2);
+    
+    soma = num1 + num2;
+    
+    printf(" O resultado é %d", soma);
+
+    return 0;
+}
