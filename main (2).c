fe71fff3c54c727826c6639daf34eb05651a3c94
bagriel17 <gabrielbastosmelo7@gmail.com>
2024-08-10 18:18:14 -0300

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,21 @@
+//CONVERSÃO HORA PARA MINUTOS - PROGRAMAÇÃO (15/40)
+
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+
+    float segundos, minutos;
+    
+    printf("Digite o número de minutos que deseja converter em segundos: \n");
+    scanf("%f", &minutos);
+    
+    segundos = minutos * 60;
+    
+    printf("Segundos = %f", segundos);
+    
+    return 0;
+}
