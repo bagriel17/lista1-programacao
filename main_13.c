33b7d35d0bfb38b1c6b426cc054fa3d2ddc58eea
bagriel17 <gabrielbastosmelo7@gmail.com>
2024-08-10 17:31:24 -0300

    Update main_13.c

--- main_13.c
+++ main_13.c
@@ -1,11 +1,4 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby,
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
+//CALCULO DE JURO COMPOSTOS - PROGRAMAÇÃO (13/40)
 #include <stdio.h>
 #include <locale.h>
 #include <math.h>
